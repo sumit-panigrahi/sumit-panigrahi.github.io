23f6f1109fc8d3bd2b11170b4dd91e6375f76b6d
sumit-panigrahi <panigrahi.sumit@outlook.com>
2022-09-06 01:18:53 +0530

    CI INIT

--- .github/scripts/publish.sh
+++ .github/scripts/publish.sh
@@ -1,6 +1,6 @@
 echo "Preparing files to commit..."
-git --work-tree generated/storybook add --all
-git --work-tree generated/storybook commit -m "gh-pages"
+git --work-tree dist add --all
+git --work-tree dist commit -m "gh-pages"
 echo "--------------------------------------------------------------------------------"
 echo "Pusing to gh-pages"
 git push origin HEAD:gh-pages --force
